eca40cd68c57181696abcdbd3f9dcd86c20a8489
francoS18 <francosebastian.garrido@alumnos.ulagos.cl>
2023-07-04 15:45:22 -0400

    Inicio de un repositorio de JS

---
+++ variables.js
@@ -0,0 +1,11 @@
+var numero = 30;
+let n = 10;
+const x = 100;
+
+// var se utiliza para definir una variable en cualquier momento aun siendo despues de invocar en la variable.
+// let es para definir una variable antes de iniciarle, tambien es mutable.
+//  const no puede mutar y debe ser definida antes de ser invocada.
+
+console.log(nombre)
+
+var nombre = 100;
